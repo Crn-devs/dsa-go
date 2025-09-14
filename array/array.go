4746b07bf2f331efb02cf75d3eac08a5790fece7
connor <crn.develops@gmail.com>
2025-09-14 02:15:59 +0100

    array type: basic ops pop, push, len, delete

--- array/array.go
+++ array/array.go
@@ -8,6 +8,25 @@ func NewArray[T any]() *Array[T] {
 	return &Array[T]{data: []T{}}
 }
 
+func (arr Array[T]) Len() int {
+	return len(arr.data)
+}
+
+func (arr *Array[T]) Push(item T) int {
+	arr.data = append(arr.data, item)
+	return len(arr.data)
+}
+
 func (arr *Array[T]) Get(index int) T {
 	return arr.data[index]
+}
+
+func (arr *Array[T]) Pop() int {
+
+	arr.data = arr.data[0 : len(arr.data)-1]
+	return len(arr.data)
+}
+
+func (arr *Array[T]) Delete(index int) {
+	arr.data = append(arr.data[:index], arr.data[index+1:]...)
 }
--- array/array_test.go
+++ array/array_test.go
@@ -1,1 +1,18 @@
 package array
+
+import (
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	arr := NewArray[int]()
+
+	if arr.Len() != 0 {
+		t.Errorf("expected: %v got: %v", 0, len(arr.data))
+	}
+
+	arr.Push(5)
+	if arr.Len() != 1 {
+		t.Errorf("expected: %v got: %v", 1, len(arr.data))
+	}
+}
