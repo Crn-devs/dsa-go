bfd18d4a14b9cd1b1dcecf3fc8e1e38d0d3b8673
connor <crn.develops@gmail.com>
2025-09-14 03:39:36 +0100

    added tests for array type + reverse string

--- array/array_Exercises.go
+++ array/array_Exercises.go
@@ -1,1 +1,24 @@
 package array
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ReverseStringArray(s string) string {
+
+	// first check input is correct
+	if len(s) <= 0 {
+		fmt.Println("incorrect length: length should be greater than 0")
+	}
+
+	// create an array to store reversed string
+	reversed := NewArray[string]()
+
+	// run reverse loop
+	for i := len(s) - 1; i >= 0; i-- {
+		reversed.Push(string(s[i]))
+	}
+
+	return strings.Join(reversed.data, "")
+}
--- array/array_test.go
+++ array/array_test.go
@@ -8,11 +8,88 @@ func TestPush(t *testing.T) {
 	arr := NewArray[int]()
 
 	if arr.Len() != 0 {
-		t.Errorf("expected: %v got: %v", 0, len(arr.data))
+		t.Errorf("expected: %v got: %v", 0, arr.Len())
 	}
 
 	arr.Push(5)
 	if arr.Len() != 1 {
-		t.Errorf("expected: %v got: %v", 1, len(arr.data))
+		t.Errorf("expected: %v got: %v", 1, arr.Len())
+	}
+}
+
+func TestLen(t *testing.T) {
+	arr := NewArray[int]()
+
+	if arr.Len() != 0 {
+		t.Errorf("expected: %v got: %v", 0, arr.Len())
+	}
+
+	arr.Push(5)
+	arr.Push(5)
+
+	if arr.Len() != 2 {
+		t.Errorf("expected: %v got: %v", 2, arr.Len())
+	}
+
+	arr.Pop()
+
+	if arr.Len() != 1 {
+		t.Errorf("expected: %v got: %v", 1, arr.Len())
+	}
+}
+
+func TestPop(t *testing.T) {
+	arr := NewArray[int]()
+
+	if arr.Len() != 0 {
+		t.Errorf("expected: %v got: %v", 0, arr.Len())
+	}
+
+	arr.Push(5)
+	arr.Push(5)
+
+	if arr.Len() != 2 {
+		t.Errorf("expected: %v got: %v", 2, arr.Len())
+	}
+	arr.Pop()
+	arr.Pop()
+
+	if arr.Len() != 0 {
+		t.Errorf("expected: %v got: %v", 0, arr.Len())
+	}
+}
+
+func TestDelete(t *testing.T) {
+	arr := NewArray[int]()
+
+	if arr.Len() != 0 {
+		t.Errorf("expected: %v got: %v", 0, arr.Len())
+	}
+
+	arr.Push(5)
+	arr.Push(10)
+	arr.Push(15)
+
+	if arr.Len() != 3 {
+		t.Errorf("expected: %v got: %v", 3, arr.Len())
+	}
+	arr.Delete(1)
+
+	if arr.Get(1) == 10 {
+		t.Errorf("expected: %v got: %v", 15, arr.Get(1))
+	}
+}
+
+func TestGet(t *testing.T) {
+	arr := NewArray[int]()
+
+	arr.Push(5)
+	arr.Push(15)
+	arr.Push(25)
+
+	got := arr.Get(1)
+
+	if got != 15 {
+		t.Errorf("expected: %v got: %v", 15, got)
 	}
 }
