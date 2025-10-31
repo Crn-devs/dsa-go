49619e7117ef62b3640762b7cbab1d22d9f404ee
connor <crn.develops@gmail.com>
2025-10-31 04:00:17 +0000

    very basic toy hashmap implementation

---
+++ hashmap/hashmap.go
@@ -0,0 +1,70 @@
+package hashmap
+
+type Pair [2]string
+
+type Hashmap struct {
+	data [][]Pair
+}
+
+func NewHashmap(size int) *Hashmap {
+	return &Hashmap{data: make([][]Pair, size)}
+}
+
+func (h *Hashmap) hash(key string) int {
+	sum := 0
+	for _, ch := range key {
+		sum += int(ch)
+	}
+	return sum % len(h.data)
+}
+
+func (h *Hashmap) Set(key, value string) {
+
+	// create the address value using the hash func
+	address := h.hash(key)
+	// if the value at the address is nil then create a bucket for it
+	if h.data[address] == nil {
+		h.data[address] = []Pair{} // slice of key value pairs [ [key, value], [key, value] ]
+	}
+
+	// just overwrite if the key is found
+	for i, pair := range h.data[address] {
+		if pair[0] == key {
+			h.data[address][i][1] = value // overwrite existing key
+			return
+		}
+	}
+
+	// add the data at address index
+	h.data[address] = append(h.data[address], Pair{key, value})
+}
+
+func (h *Hashmap) Get(key string) string {
+	address := h.hash(key)
+
+	// get the current bucket / array from the key address
+	currentBucket := h.data[address]
+
+	if currentBucket != nil {
+		for i := 0; i < len(currentBucket); i++ {
+			if currentBucket[i][0] == key { // check the key inside the current bucket
+				return currentBucket[i][1] // return the value
+			}
+		}
+	}
+	return ""
+}
+
+func (h *Hashmap) Keys() []string {
+	var keys []string
+	for i := 0; i < len(h.data); i++ {
+		bucket := h.data[i]
+		if bucket == nil {
+			continue
+		}
+		for _, pair := range bucket {
+			keys = append(keys, pair[0])
+		}
+	}
+	return keys
+}
