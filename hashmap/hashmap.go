e854c7a69d9c19385abd4fc0472b01fd93e69ced
connor <crn.develops@gmail.com>
2025-11-01 03:20:22 +0000

    hashmap struct impovement in progress

--- hashmap/hashmap.go
+++ hashmap/hashmap.go
@@ -1,5 +1,58 @@
 package hashmap
 
+// re-implemented after deep dive
+type bucketNode struct {
+	key   string
+	value string
+	next  *bucketNode
+}
+
+type bucket struct {
+	head *bucketNode
+}
+
+type HashTable struct {
+	buckets []*bucket
+	size    int
+}
+
+func (h *HashTable) NewHashTable(size int) *HashTable {
+	return &HashTable{
+		buckets: make([]*bucket, size),
+		size:    size,
+	}
+}
+
+func (h *HashTable) hash(key string) int {
+	sum := 0
+	for _, ch := range key {
+		sum += int(ch)
+	}
+	return sum % len(h.buckets)
+}
+
+func (h *HashTable) Set(key, value string) {
+
+	// create the address value using the hash func
+	address := h.hash(key)
+	// if the value at the address is nil then create a bucket for it
+	if h.buckets[address] == nil {
+		h.buckets[address] = &bucket{} // slice of key value pairs [ [key, value], [key, value] ]
+	}
+
+	// just overwrite if the key is found
+
+	h.buckets[address].insert(key)
+}
+
+func (b *bucket) insert(k string) {
+	newNode := &bucketNode{key: k}
+	newNode.next = b.head
+	b.head = newNode
+}
+
+// Un-guided implementation // little messy overall
+
 type Pair [2]string
 
 type Hashmap struct {
@@ -30,7 +83,7 @@ func (h *Hashmap) Set(key, value string) {
 	// just overwrite if the key is found
 	for i, pair := range h.data[address] {
 		if pair[0] == key {
-			h.data[address][i][1] = value // overwrite existing key
+			h.data[address][i][1] = value // overwrite existing value
 			return
 		}
 	}
