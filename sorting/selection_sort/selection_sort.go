59a3f1b22cd0a13ef6483c13047f809c227e495c
connor <crn.develops@gmail.com>
2026-01-01 04:37:57 +0000

    little cleanup

--- sorting/selection_sort/selection_sort.go
+++ sorting/selection_sort/selection_sort.go
@@ -2,7 +2,6 @@ package selectionsort
 
 import (
 	"cmp"
-	"fmt"
 )
 
 func SelectionSort[S ~[]E, E cmp.Ordered](s S) S {
@@ -10,18 +9,14 @@ func SelectionSort[S ~[]E, E cmp.Ordered](s S) S {
 	var min int
 
 	for i := 0; i < len(s)-1; i++ {
-		// set min to be the first element
+		// set min to be the first elements index
 		min = i
-		fmt.Println("min is:", min)
-
 		for j := i + 1; j < len(s); j++ {
-			// check the first element (min) againt the rest of the slice
+			// check the first element the element at index(min) againt the rest of the slice
 			// if min > next element swap min
 			if s[j] < s[min] {
 				min = j
 			}
-
-			fmt.Println("min is:", min)
 		}
 
 		s[i], s[min] = s[min], s[i]
